96dfb21dc796bb497df319b5ed6c68cc40f370b4
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-11 18:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
 
-  else (string.toLowercase() === string) {
+  else if (string.toLowerCase() === string) {
     return "I can\'t hear you!"
   }
 
-  else (string === "I love you, Grandma") {
+  else if (string === "I love you, Grandma") {
     return "I love you, too."
   }
 }
